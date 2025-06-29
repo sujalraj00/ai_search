ac9d73ec90c9d4ba524f5705f9c1464a88b772f9
Sujal Raj <sujalraj611@gmail.com>
2025-06-29 20:29:44 +0530

    initial repo

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          // side navbar
+          // column
+          Column(
+            children: [
+              //search bar
+              // footer
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
