65fa69573fc429d602b60e1895a11ee3b438466d
Sujal Raj <sujalraj611@gmail.com>
2025-07-29 15:52:08 +0530

    llm model changed

--- server/services/llm_service.py
+++ server/services/llm_service.py
@@ -6,7 +6,7 @@ settings = Settings()
 class LLMService:
     def __init__(self):
         genai.configure(api_key=settings.GEMINI_API_KEY)
-        self.model = genai.GenerativeModel("gemini-2.0-flash")
+        self.model = genai.GenerativeModel("gemini-2.5-flash")
 
     def generate_response(self, query: str, search_results: list[dict]):
         #source1 <url>
