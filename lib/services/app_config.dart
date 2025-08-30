9d4838c25414af15406f19ecadc68a730c734123
Sujal Raj <sujalraj611@gmail.com>
2025-08-30 22:29:50 +0530

    requitement file for server updated

--- lib/services/app_config.dart
+++
@@ -1,5 +0,0 @@
-class AppConfig {
-  static const String geminiApiKey =
-      'AIzaSyDJaB2NNUe1sf1-3AGIzNLwR65NbIZBCB0'; // Replace with your actual API key
-  static const String mcpServerUrl = 'https://mcp-x-server.onrender.com/sse';
-}
